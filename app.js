99b125bb012e122d03af04320530398f918e3ed6
Flora2020 <rossignols1989@gmail.com>
2021-01-31 20:14:46 +0800

    feat: preserve login status

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const session = require('express-session')
 
 const User = require('./models/User.js')
 
@@ -23,26 +24,43 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'oeni93u4bcfmjex,a',
+  resave: false,
+  saveUninitialized: false
+}))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/login/test', (req, res) => {
+  const email = req.session.email
+  return User.find({ email })
+    .lean()
+    .then(user => {
+      if (!user.length) { res.render('fail') }
+      res.render('test', { user: user[0] })
+    })
+    .catch(error => console.log(error))
+})
+
 app.post('/login', (req, res) => {
   const { email, password } = req.body
-  return User.find({ email: email })
+  return User.find({ email })
     .lean()
     .then(user => {
       if (user[0].password === password) {
+        req.session.email = email
         const firstName = user[0].firstName
         res.render('welcome', { firstName })
       } else {
-        res.redirect('/')
+        res.render('fail')
       }
       return
     })
     .catch(error => {
-      res.redirect('/')
+      res.render('fail')
       console.log(error)
       return
     })
